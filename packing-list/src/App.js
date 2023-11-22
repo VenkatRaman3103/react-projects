f1621a59715cf9867a70ac0898a1807fe73edcff
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2023-11-22 12:01:12 +0530

    Update

--- packing-list/src/App.js
+++ packing-list/src/App.js
@@ -1,5 +1,10 @@
 import { useState } from "react";
 
+const initialItems = [
+    { id: 1, description: "Passports", quantity: 2, packed: false },
+    { id: 2, description: "Socks", quantity: 12, packed: false },
+  ];
+
 function App() {
     return (
         <div>
