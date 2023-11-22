ea5fd509fd9ec9c1f495708470469a145091d19f
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2023-11-22 10:26:09 +0530

    Update

--- packing-list/src/App.js
+++ packing-list/src/App.js
@@ -1,8 +1,27 @@
 import { useState } from "react";
 
 function App() {
-  return 
-    
+    return (
+        <div>
+            <Title />
+            <Form />
+            <ItemsList />
+            <Footer />
+        </div>
+    );
 }
+
+function Title() {
+    return (
+        <div>
+            <h1>Travel App</h1>
+        </div>
+    );
+}
+function Form() {
+    return;
+}
+function ItemsList() {}
+function Footer() {}
 
 export default App;
