c0e2c50dcafa4ad19a9a30af6d80659c56d072d1
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2023-11-22 11:02:06 +0530

    Update form branch

--- packing-list/src/App.js
+++ packing-list/src/App.js
@@ -19,7 +19,18 @@ function Title() {
     );
 }
 function Form() {
-    return;
+    return (
+        <form className="add-form"> 
+            <h2>Enter the itmes: </h2>
+            <select>
+                {Array.from({ length: 10 }, (_, i) => i + 1).map((num) => (
+                    <option>{num}</option>
+                ))}
+            </select>
+            <input placeholder="items..."></input>
+            <button>Add</button>
+        </form>
+    );
 }
 function ItemsList() {}
 function Footer() {}
